aa8dfa59a4c852cbae8be6505dd32a729bb46786
Josh Bernd <joshbernd@Joshs-MacBook-Pro.local>
2020-11-01 23:52:17 -0800

    added walkthrough and updated src files

--- src/helpers.py
+++ src/helpers.py
@@ -1,42 +1,185 @@
 import numpy as np
 import pandas as pd
 
+
+def drop_nans(df, ax, how):
+    '''
+        Drops rows or collumns from a that contain nans
+        
+        ARGS:
+            df - pandas dataframe
+            ax - 0 for row, 1 for column
+            how - 'all' if enire ax is nans, 'any' if less than all
+        Return:
+            df - reduced dataframe
+    '''
+    df = df.dropna(axis=ax, how=how)
+    return df
+
+def get_impaired_waters_types(df, col):
+    body_dict = {}
+    df[col] = df[col].fillna('Null')
+
+    for body in df[col]:
+        if body in body_dict:
+            body_dict[body] += 1
+        else:
+            body_dict[body] = 1
+    
+    body_list = []
+    amount_list = []
+    for k, v in body_dict.items():
+        body_list.append(k)
+        amount_list.append(int(v))
+    
+    return [body_list, amount_list]
+
+
+
+def fill_nans(df, col, replace_with):
+    d = {}
+    df['col'] = df['col'].fillna(replace_with)
+
+    for body in df['col']:
+        if body in d:
+            d[body] += 1
+        else:
+            d[body] = 1
+
+    body_list = []
+    amount_list = []
+    for k, v in body_dict.items():
+        body_list.append(k)
+        amount_list.append(int(v))
+    return [body_list, amount_list]
+
+
+def filter(df, col, text):
+    df = df.loc[df[col] != text]
+    return df
+
+def clean_waters_data(waters):
+    waters = waters.dropna(axis=0, how='all')
+    rivers = waters.loc[waters['WATER BODY TYPE'] == 'River & Stream']
+    rivers = rivers[['Region', 'Water Body Name', 'POLLUTANT']]
+    rivers['Water Body Name'] = rivers['Water Body Name'].str.lower()
+
+    to_numeric(rivers, ['Region'])
+    rivers.index = np.arange(0, len(rivers))
+
+
+    return rivers
+
+
+
+def clean_dams(dams):
+    dams = dams.rename(columns={'RIVER': 'River', 'PRIVATE_DAM': 'Private Dam'})
+
+    dams = dams[['River', 'Private Dam']]
+    dams = dams[dams['River'].notna()]
+    dams['River'] = dams['River'].str.lower()
+    dams = filter(dams, 'River', 'offstream')
+    dams = filter(dams, 'River', 'y')
+    dams = filter(dams, 'River', '.')
+    dams = filter(dams, 'River', 3)
+
+    dams = dams.drop_duplicates(subset='River', keep="first")
+
+    return dams
+
+
+def reset_index(df):
+    df.index = np.arange(0, len(df))
+
 def to_numeric(df, cols):
     for col in cols:
         df[col] = df[col].apply(lambda x: pd.to_numeric(x, errors='coerce'))
 
-def to_str(df, cols):
-    for col in cols:
-        df[col] = df[col].apply(lambda x: pd.to_string(x, errors='coerce'))
-        
-
-
-
-
-def get_impaired_dams(dams, impaired_waters):
-    impaired_dict = {}
-    for dam in dams['RIVER']:
-        for river in impaired_waters['Water Body Name']:
-            if river.str.contains('|'.join(dam)) == True:
-                impaired_dict[dam] = river
-
     
-    return impaired_dict
+def condense(df):
+    df['All Pollutants'] = df[[
+    'Water Body Name',
+    'POLLUTANT']].groupby('Water Body Name')['POLLUTANT'].transform(lambda x: ', '.join(x))
+
+    df = df[['Water Body Name', 'All Pollutants' , 'Region']].drop_duplicates()
+    return df
+
+
+def add_col_num_pollutants(df):
+
+    df['Number of Pollutants'] = 0
+    for i in range (df['All Pollutants'].count()):
+        pollutant_list = df['All Pollutants'][i].split(', ')
+        pollutant_list = list(dict.fromkeys(pollutant_list))
+        df['All Pollutants'][i] = pollutant_list
+        df['Number of Pollutants'][i] = len(pollutant_list)
+
+    return df
+
+
+def merge_dfs(df1, df2, left, right):
+    df1 = df1.set_index('Water Body Name', drop=False)
+    df1 = df1.rename(columns={'Water Body Name': 'River'})
+
+    for impaired_river in df1['River']:
+    
+        for dammed_river in df2['River']:
+            if dammed_river in impaired_river:
+                df1['River'].loc[df1['River'] == impaired_river] = dammed_river
+                impaired_river = dammed_river
+                break
+
+    df = df2.groupby(['River'], sort=False)['Private Dam'].apply(lambda x: ','.join(x.astype(str)))
+    df = df1.merge(df2, how='left', left_on=left, right_on=right)
+    
+    return df
 
 
 
+def bootstrap_sample_means(data, n_bootstrap_samples=10000):
+    bootstrap_sample_means = []
+    for i in range(n_bootstrap_samples):
+        bootstrap_sample = np.random.choice(data, size=300, replace=True)  ##changed from size=len(data)
+        bootstrap_sample_means.append(np.mean(bootstrap_sample))
+    
+    return bootstrap_sample_means
 
-#pd.DataFrame.from_dict(data, orient='index',
-#                       columns=['A', 'B', 'C', 'D'])
 
-#dams_data['impaired'] = [True if ]
-#searchfor = dams_data['RIVER']
-#impaired_rivers['Water Body Name'][26].str.contains('|'.join(dams_data['RIVER'][100]))
-#s = impaired_rivers['Water Body Name']
+def combine_dicts(d1, d2):
+    d3 = d1
+    for k, v in d2.items():
+        if k not in d3:
+            d3[k] = v
+    return d3
 
-def the_dammed_and_impaired(dams, impaired_waters):
+def solvability(most_potent_dict, treatable):
+    solvable = 0
+    unsolvable = 0
+    for k, v in most_potent_dict.items():
+        if treatable[k] == 'Yes':
+            solvable += v
+        else:
+            unsolvable += v
+    return [solvable, unsolvable]
 
-    ##how to find dammed impaired rivers?
-    #1. If 'dam' in river's name
-    #2. If 'dam' construction in possible causes
-    #3. If dams_data.RIVER in impaired_rivers.name & state in states in region
+
+def get_pollutant_dict(df):
+    pollutant_dict = {}
+    for pol in df['All Pollutants']:
+        for pollutant in pol:
+            if pollutant in pollutant_dict:
+                pollutant_dict[pollutant] += 1
+            else:
+                pollutant_dict[pollutant] = 1
+            
+    return pollutant_dict
+
+
+def get_first_n(d, n):
+    top_n = {}
+    for i in range(n):
+        d_copy = d
+        largest_key = max(d_copy, key=d_copy.get)
+        top_n[largest_key] = d_copy[largest_key]
+        del d_copy[largest_key]
+    return top_n
---
+++ src/plot.py
@@ -0,0 +1,141 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+colors_list = ['#71C348', '#48A9C3']
+
+
+plt.style.use('ggplot')
+
+def plot_water_bodies(lst, title='Counts of Water Body Types in 303(d) Data', size=(14, 5)):
+    '''
+        Prints a barchart of entries per water body in 303(d) list
+        
+        ARGS:
+            x - list
+            y - list
+        Return:
+            ax - axis with plot
+    '''
+
+    fig, ax = plt.subplots(figsize=size)
+    plt.title(title)
+    
+    x_pos = [i for i, _ in enumerate(lst[0])]
+    plt.xticks(x_pos, lst[0])
+    plt.setp(plt.gca().get_xticklabels(), rotation=30, horizontalalignment='right')
+    plt.bar(lst[0], lst[1])
+
+    plt.xlabel("Water Body Type")
+    plt.ylabel("Number of Entries")
+    plt.title("Counts of Water Body Types in 303(d) Data")
+    return ax
+
+
+
+def plot_most_potent_stacked_bar(data_dict):
+    fig, ax = plt.subplots(figsize=(9, 7))
+    r = [0,1]
+
+    df = pd.DataFrame(data_dict)
+    
+    # From raw value to percentage
+    totals = [i+j for i,j in zip(df['Dammed'], df['Natural'])]
+    dammed = [i / j * 100 for i,j in zip(df['Dammed'], totals)]
+    natural = [i / j * 100 for i,j in zip(df['Natural'], totals)]
+    
+    # plot
+    barWidth = 0.65
+    names = ('Dammed', 'Natural')
+
+    plt.bar(r, dammed, color=colors_list[0], edgecolor='white', width=barWidth)
+    # Create blue Bars
+    plt.bar(r, natural, bottom=[i for i in dammed], color=colors_list[1], edgecolor='white', width=barWidth)
+    
+    # Custom x axis
+    plt.xticks(r, names)
+
+    plt.ylabel('Percent Solvable')
+    plt.title('Proportion of Pollutants Solvable with Implementation of TMDLs')
+    colors = {'% Unsolvable with TMDLs':colors_list[0], '% Solvable with TMDLs':colors_list[1]}
+    labels = ['% Unsolvable with TMDLs', '% Solvable with TMDLs']
+
+    handles = [plt.Rectangle((0,0),1,1, color=colors[label]) for label in labels]
+    plt.legend(handles, labels)
+        
+    plt.show()
+
+
+
+def plot_most_common_pollutants(dammed_most_potent, undammed_most_potent):
+
+    x_list = []
+    for k, v in dammed_most_potent.items():
+        if k not in x_list:
+            x_list.append(k)
+        
+    for k, v in undammed_most_potent.items():
+        if k not in x_list:
+            x_list.append(k)
+    x_list = sorted(x_list)
+
+    dammed = []
+    for item in x_list:
+        if item in dammed_most_potent:
+            dammed.append(dammed_most_potent[item])
+        else:
+            dammed.append(0.5)
+    undammed = []
+    for item in x_list:
+        if item in undammed_most_potent:
+            undammed.append(undammed_most_potent[item])
+        else:
+            undammed.append(0.5)
+            
+    fig, ax = plt.subplots(figsize=(20, 5))
+            
+    N = len(x_list)
+    ind = np.arange(N)
+    width = 0.4
+        
+    ax.bar(ind, dammed, width, color=colors_list[0], label='Dammed')
+    ax.bar(ind + width , undammed, width, color=colors_list[1],
+        label='Undammed')
+
+    plt.ylabel('# of Occurances of Impairment')
+    plt.title('Top 15 Impairments in Dammed and Undammed Rivers')
+
+    plt.xticks(ind + width / 2, x_list)
+    plt.setp(plt.gca().get_xticklabels(), rotation=30, horizontalalignment='right')
+    plt.legend(loc='best')
+    plt.show()
+
+
+def plot_simple_bar(figsize, dict_list, title, ylabel, labels):
+
+    fig, ax = plt.subplots(figsize=figsize)
+    i = 0
+    for d in dict_list:
+        plt.bar(*zip(*d.items()), color=colors_list[i])
+        i += 1
+
+
+    plt.title(title)    
+    plt.setp(plt.gca().get_xticklabels(), rotation=30, horizontalalignment='right')
+    plt.ylabel(ylabel)
+
+    #set up colors dictionary to manually create a legend
+    colors = {}
+
+    i = 0
+    for l in labels:
+        colors[l] = colors_list[i]
+        i += 1
+    
+    labels = labels
+    #create a legend
+    rects = [plt.Rectangle((0,0),1,1, color=colors[label]) for label in labels]
+    plt.legend(rects, labels)
+    plt.show()
+
+
